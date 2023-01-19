e20acbd74462984eed4c22ddecc7a4b630501cc0
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-01-19 17:26:05 +0000

    Additional code commit Added a pages for a gallery style collections of earrings called from a web api

---
+++ EmilyLilyCreationsWebShop/Models/EarringModel.cs
@@ -0,0 +1,17 @@
+﻿namespace EmilyLilyCreationsWebShop.Models
+{
+    public class EarringModel
+    {
+        public string partitionKey { get; set; }
+        public string rowKey { get; set; }
+        public DateTime timestamp { get; set; }
+        public float price { get; set; }
+        public string description { get; set; }
+        public string colour { get; set; }
+        public string imageUrl { get; set; }
+        public int quantityPairs { get; set; }
+        public string name { get; set; }
+        public string materials { get; set; }
+        public string size { get; set; }
+    }
+}
--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -2,10 +2,12 @@ using EmilyLilyCreationsWebShop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+const string BaseImageUrl = "https://dalkeithgh.blob.core.windows.net/emilylilycreations/";
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://emilylilywebshop.azurewebsites.net/api/") });
 
 await builder.Build().RunAsync();
