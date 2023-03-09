f75e89435f411433719d39f13ae4aff5429fed28
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-03-09 17:36:43 +0000

    Testing
    Testing some environment variables.

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 const string BaseImageUrl = "https://dalkeithgh.blob.core.windows.net/emilylilycreations/";
+string BaseApiUrl = Environment.GetEnvironmentVariable("API_url");
+string BaseBlobUrl = Environment.GetEnvironmentVariable("BlobStorage_url");
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
