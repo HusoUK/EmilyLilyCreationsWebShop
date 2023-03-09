b9353edbd5537f2e4c4dd064e47d8e62688b343a
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-03-09 18:35:15 +0000

    Reverted last changes

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -11,6 +11,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(BaseApiUrl) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://emilylilywebshop.azurewebsites.net/api/") });
 
 await builder.Build().RunAsync();
