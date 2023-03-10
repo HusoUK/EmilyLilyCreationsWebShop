8bb8178d43397782eb501b7af3219d42082421ef
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-03-10 16:40:44 +0000

    Testing
    Testing environment variables.

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -2,15 +2,20 @@ using EmilyLilyCreationsWebShop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-const string BaseImageUrl = "https://dalkeithgh.blob.core.windows.net/emilylilycreations/";
-string BaseApiUrl = Environment.GetEnvironmentVariable("WEBSITE_API_url");
-string BaseBlobUrl = Environment.GetEnvironmentVariable("WEBSITE_BlobStorage_url");
+//Testing the below for accessing environment variables
+//const string BaseImageUrl = "https://dalkeithgh.blob.core.windows.net/emilylilycreations/";
+//string BaseApiUrl = Environment.GetEnvironmentVariable("WEBSITE_API_url");
+//string BaseBlobUrl = Environment.GetEnvironmentVariable("WEBSITE_BlobStorage_url");
 
+var configurationBuilder = new ConfigurationBuilder();
+var configuration = configurationBuilder.Build();
+
+var apiUrl = configuration["WEBSITE_API_url"];
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://emilylilywebshop.azurewebsites.net/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
