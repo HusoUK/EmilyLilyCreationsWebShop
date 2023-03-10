d17c3b7def2ae194399e85a8074620044693da83
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-03-10 17:10:22 +0000

    Reverting changes.

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -7,15 +7,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 //string BaseApiUrl = Environment.GetEnvironmentVariable("WEBSITE_API_url");
 //string BaseBlobUrl = Environment.GetEnvironmentVariable("WEBSITE_BlobStorage_url");
 
-var configurationBuilder = new ConfigurationBuilder();
-var configuration = configurationBuilder.Build();
+//var configurationBuilder = new ConfigurationBuilder();
+//var configuration = configurationBuilder.Build();
 
-var apiUrl = configuration["WEBSITE_API_url"];
+//var apiUrl = configuration["WEBSITE_API_url"];
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://emilylilywebshop.azurewebsites.net/api/") });
 
 await builder.Build().RunAsync();
