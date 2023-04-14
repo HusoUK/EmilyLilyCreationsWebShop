4848eee93c5b36fe0cd47a2001e0f63abe289bf1
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-04-14 13:52:33 +0100

    New updates
    new updates and content.

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -2,16 +2,6 @@ using EmilyLilyCreationsWebShop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-//Testing the below for accessing environment variables
-//const string BaseImageUrl = "https://dalkeithgh.blob.core.windows.net/emilylilycreations/";
-//string BaseApiUrl = Environment.GetEnvironmentVariable("WEBSITE_API_url");
-//string BaseBlobUrl = Environment.GetEnvironmentVariable("WEBSITE_BlobStorage_url");
-
-//var configurationBuilder = new ConfigurationBuilder();
-//var configuration = configurationBuilder.Build();
-
-//var apiUrl = configuration["WEBSITE_API_url"];
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
