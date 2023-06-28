6c17874f617161326cd2afc0b4e4b2c51d5639c6
Richard Wilkinson <richwilk.iphone@gmail.com>
2023-06-28 15:08:35 +0100

    Update/ Fix
    Updated the contact form, so it now sends the users message on to an email address.
    
    Fixed a CSS rule, so now the gallery page displays correctly on small screens.
    
    Also moved the base API url to an appsetting.json file.

--- EmilyLilyCreationsWebShop/Program.cs
+++ EmilyLilyCreationsWebShop/Program.cs
@@ -1,4 +1,5 @@
 using EmilyLilyCreationsWebShop;
+using EmilyLilyCreationsWebShop.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://emilylilywebshop.azurewebsites.net/api/") });
+var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+builder.Services.AddScoped< EmailMessage>();
 
 await builder.Build().RunAsync();
